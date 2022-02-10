93693f98ad6c9060455d868182ee5c81c3a9800f
Wes Garcia <wes@alltradestech.com>
2022-02-09 19:38:01 -0800

    Add comments

--- challenge/index.md
+++ challenge/index.md
@@ -4,3 +4,6 @@
 3. Great Expectations
 4. The Gunslinger
 5. The Ocean at the End of the Lane
+
+
+For this challenge I was paired with Matt D. I shared my screen, but we each worked in our own repo simultaneously, taking the same steps together as we went through the instructions. If either one of us had a question it was easy to just ask, "How would we do this again? ...Ok, yeah, thanks!"
--- challenge/pc.rb
+++ challenge/pc.rb
@@ -1,10 +1,13 @@
 # I called this file pc.rb for "paired challenge" before I knew it would be about books. haha!
 
-# variable nearest_library declared and assigned to value "Santa Clarita Library"
+# variable nearest_library declared and assigned to string value "Santa Clarita Library"
 nearest_library = "Santa Clarita Library"
+# variable num_of_books declared and assigned to integer value 1200
 num_of_books = 1200
+# variable library_open declared and assigned boolean value false
 library_open = false
 returned_book = true
 checkout_books = 200
 most_borrowed = "Hitchhiker's Guide to the Galaxy"
+# variable scifi_books stores an array of strings. this array has 3 books.
 scifi_books = ["Hunger Games", "Star Wars", "Dragonheart"]
