d0a9612eb802b977385ea6ae71a081ad75ea9017
Wes Garcia <wes@alltradestech.com>
2022-02-09 19:22:38 -0800

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+# Book Titles
+1. Learn Ruby the Hard Way
+2. Learn Python the Hard Way
+3. Great Expectations
+4. The Gunslinger
+5. The Ocean at the End of the Lane
---
+++ challenge/pc.rb
@@ -0,0 +1,10 @@
+# I called this file pc.rb for "paired challenge" before I knew it would be about books. haha!
+
+# variable nearest_library declared and assigned to value "Santa Clarita Library"
+nearest_library = "Santa Clarita Library"
+num_of_books = 1200
+library_open = false
+returned_book = true
+checkout_books = 200
+most_borrowed = "Hitchhiker's Guide to the Galaxy"
+scifi_books = ["Hunger Games", "Star Wars", "Dragonheart"]
